7b2ae92a51f35fa9b414f71a192957911bf7ae9a
SayeddilawarG <sayed.dilawarg@gmail.com>
2025-10-13 21:48:58 +0530

    added google api key

---
+++ src/utils/contants.js
@@ -0,0 +1,12 @@
+const GOOGLE_API_KEY = "AIzaSyBIu5pi0llsoD7tMeZ2nbXz_oVuI9BEYOc";
+
+export const LIVE_CHAT_COUNT = 25;
+
+export const YOUTUBE_VIDEOS_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" +
+  GOOGLE_API_KEY;
+
+export const YOUTUBE_SEARCH_API =
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+// Live Chat >>>> Infinite Scroll >>>>>> Pagination
